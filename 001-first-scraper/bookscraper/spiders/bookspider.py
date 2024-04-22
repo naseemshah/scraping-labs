c098a49d1a1582cd998d14184646e8894fa695f4
Naseem Shah <naseemshahvarikodan@gmail.com>
2024-04-22 14:55:01 +0530

    feat: scrape all the fields from nested book page

--- 001-first-scraper/bookscraper/spiders/bookspider.py
+++ 001-first-scraper/bookscraper/spiders/bookspider.py
@@ -26,4 +26,21 @@ class BookspiderSpider(scrapy.Spider):
             yield response.follow(next_page_url, callback = self.parse)
 
     def parse_book_page(self, response):
-        pass
+        table_rows = response.css("table tr")
+        
+        yield{
+            
+            'url': response.url,
+            'title': response.css('.product_main h1::text').get(),
+            'product_type': table_rows[1].css("td ::text").get(),      
+            'price_excl_tax': table_rows[2].css("td ::text").get(),
+            'price_incl_tax': table_rows[3].css("td ::text").get(),     
+            'tax': table_rows[4].css("td ::text").get(),
+            'availability': table_rows[5].css("td ::text").get(),
+            'num_reviews': table_rows[6].css("td ::text").get(),
+            'stars': response.css("p.star-rating").attrib['class'],
+            'category':response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+            'description':response.xpath("//div[@class='product_description']/following-sibling::p/text()").get(),
+            'price':response.css("p.price_color ::text").get()
+
+        }
